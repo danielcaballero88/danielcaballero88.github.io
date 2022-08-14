f1f5dfd1e1a0376ee6ce1b36e6c430314f4d330f
Daniel Caballero <danielcaballero88@gmail.com>
2022-08-14 16:58:52 +0200

    Add header component
      Header component added with just some icons.
    
      To add the icons, they are added to the assets/icons directory and
      included in the MatIconRegistry, with exceptions for the DomSanitizer.

--- my-website/src/app/app.component.ts
+++ my-website/src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'my-website';
+
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer,
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      "twitter",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../../assets/icons/twitter.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "linkedin",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../../assets/icons/linkedin.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "github",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../../assets/icons/github.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "stackoverflow",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../../assets/icons/stackoverflow.svg")
+    );
+  }
 }
--- my-website/src/app/app.module.ts
+++ my-website/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { MatIconModule, MatIconRegistry } from "@angular/material/icon";
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatIconModule,
+    MatToolbarModule,
   ],
-  providers: [],
+  providers: [MatIconRegistry],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
