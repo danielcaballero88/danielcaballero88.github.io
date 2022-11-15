4d8e248ec62623c465fd67061a27de0ef1eddd9d
Daniel Caballero <danielcaballero88@gmail.com>
2022-11-15 20:11:43 +0700

    Start implementing navbar
      The idea is to style it as mobile first, and eventually add responsive
      design for wider screens.
      The nav items (links) are added and styled, and a bars button added
      for toggling the menu.
      Need to add animation for the menu.

--- dc-website/src/app/components/navbar/navbar.component.ts
+++ dc-website/src/app/components/navbar/navbar.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  public menuOpen: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
