1bc1dbbb7e5547b0dc738a419479c4d8935d412e
Daniel Caballero <danielcaballero88@gmail.com>
2022-11-16 11:02:53 +0700

    Improve menu animations
      The menu items are translated to the right so they come from the top
      right when the menu is open, but then some delay is added to the
      animations of the menu items so they end up coming from the right
      and each one at a time.

--- dc-website/src/app/components/navbar/navbar.component.ts
+++ dc-website/src/app/components/navbar/navbar.component.ts
@@ -8,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
   public menuOpen: boolean = false;
 
+  public linkList: { label: string; url: string }[] = [
+    { label: 'Home', url: '#' },
+    { label: 'About Me', url: '#' },
+    { label: 'My Projects', url: '#' },
+    { label: 'Contact Me', url: '#' },
+  ];
+
   constructor() {}
 
   ngOnInit(): void {}
