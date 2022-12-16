f42668d04ae1e5011fa2782a1ab2fae986c782d8
Daniel Caballero <danielcaballero88@gmail.com>
2022-12-16 20:20:17 +0700

    Add projects component

--- dc-website/src/app/app-routing.module.ts
+++ dc-website/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from 'src/app/components/about/about.component';
 import { HomeComponent } from 'src/app/components/home/home.component';
+import { ProjectsComponent } from 'src/app/components/projects/projects.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'projects', component: ProjectsComponent },
   { path: '**', redirectTo: '' },
 ];
 
--- dc-website/src/app/app.module.ts
+++ dc-website/src/app/app.module.ts
@@ -6,9 +6,16 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, HomeComponent, AboutComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    ProjectsComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
