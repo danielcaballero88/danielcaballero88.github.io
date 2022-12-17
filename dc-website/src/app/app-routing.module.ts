20d4bee9a85afd0046977dcc63c5694ab10aaa50
Daniel Caballero <danielcaballero88@gmail.com>
2022-12-17 17:10:28 +0700

    Implement contact page

--- dc-website/src/app/app-routing.module.ts
+++ dc-website/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from 'src/app/components/about/about.component';
+import { ContactComponent } from 'src/app/components/contact/contact.component';
 import { HomeComponent } from 'src/app/components/home/home.component';
 import { ProjectsComponent } from 'src/app/components/projects/projects.component';
 
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactComponent },
   { path: '**', redirectTo: '' },
 ];
 
