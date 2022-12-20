fecb1d4238cbb8bb217b167cfc03a7a870357be8
Daniel Caballero <danielcaballero88@gmail.com>
2022-12-20 17:57:40 +0700

    Fix calculation of --app-height

--- dc-website/src/app/app.component.ts
+++ dc-website/src/app/app.component.ts
@@ -1,14 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { fromEvent, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'dc-website';
 
-  ngOnInit() {
+  // Add listener to re-calculate --app-height whenever the window is resized.
+  resizeObservable$: Observable<Event> = fromEvent(window, 'resize');
+  resizeSubscription$: Subscription = this.resizeObservable$.subscribe(
+    (evt) => {
+      this.calculateAppHeight();
+    }
+  );
+
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.calculateAppHeight();
+  }
+
+  calculateAppHeight() {
     const body = document.querySelector('body');
     if (body) {
       body.style.setProperty('--app-height', `${window.innerHeight}px`);
