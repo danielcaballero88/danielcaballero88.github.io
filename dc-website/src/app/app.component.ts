11d222db60f58df75fd133e88c500b5c333708c4
Daniel Caballero <danielcaballero88@gmail.com>
2022-12-20 13:22:36 +0700

    Fix 100vh for mobile devices

--- dc-website/src/app/app.component.ts
+++ dc-website/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dc-website';
+
+  ngOnInit() {
+    const body = document.querySelector('body');
+    if (body) {
+      body.style.setProperty('--app-height', `${window.innerHeight}px`);
+    }
+  }
 }
