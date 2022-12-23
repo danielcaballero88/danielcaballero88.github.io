d297a54f69db2e7edd4fbe48960915a5e58e075b
Daniel Caballero <danielcaballero88@gmail.com>
2022-12-23 12:46:03 +0700

    Change projects order and shorten titles

--- dc-website/src/app/components/projects/projects.component.ts
+++ dc-website/src/app/components/projects/projects.component.ts
@@ -9,55 +9,7 @@ import { ProjectObj } from 'src/app/models/projects.model';
 export class ProjectsComponent implements OnInit {
   public projectObjs: ProjectObj[] = [
     {
-      title: 'How to Spin Up a VM in GCP',
-      linkObjs: [
-        {
-          label: 'Read Post',
-          url: 'https://dev.to/danielcaballero88/how-to-spin-up-a-free-linux-virtual-machine-on-google-cloud-platform-1olj',
-          icon: 'dev',
-        },
-      ],
-      imageObj: {
-        alt: 'Cloud Computing Concept Image',
-        src: '/assets/images/projects/cloud-computing.jpg',
-        attributionLabel: 'Image by Vecteezy',
-        attributionUrl: 'https://www.vecteezy.com/free-vector/cloud-computing',
-      },
-    },
-    {
-      title: 'How to Set Up Notifications in Python',
-      linkObjs: [
-        {
-          label: 'Read Post',
-          url: 'https://dev.to/danielcaballero88/how-to-set-up-email-and-message-notifications-for-a-python-application-5f3l',
-          icon: 'dev',
-        },
-      ],
-      imageObj: {
-        alt: 'Notifications Concept Image',
-        src: '/assets/images/projects/notifications.jpg',
-        attributionLabel: 'Image by Vecteezy',
-        attributionUrl: 'https://www.vecteezy.com/free-photos',
-      },
-    },
-    {
-      title: 'How to deploy to a Linux VM',
-      linkObjs: [
-        {
-          label: 'Read Post',
-          url: 'https://dev.to/danielcaballero88/how-to-deploy-your-program-to-run-periodically-in-a-linux-vm-in-gcp-compute-engine-4km6',
-          icon: 'dev',
-        },
-      ],
-      imageObj: {
-        alt: 'Deploy to Cloud Concept',
-        src: '/assets/images/projects/deploy-to-cloud.jpg',
-        attributionLabel: 'Image by Vecteezy',
-        attributionUrl: 'https://www.vecteezy.com/free-vector/cloud-computing',
-      },
-    },
-    {
-      title: 'How to Implement a Simple Python Scraper',
+      title: 'Simple Python Scraper',
       linkObjs: [
         {
           label: 'Read Post',
@@ -73,6 +25,54 @@ export class ProjectsComponent implements OnInit {
       imageObj: {
         alt: 'Implement a Python Scraper Concept',
         src: '/assets/images/projects/python-scraper.jpg',
+      },
+    },
+    {
+      title: 'Spin Up a VM in GCP',
+      linkObjs: [
+        {
+          label: 'Read Post',
+          url: 'https://dev.to/danielcaballero88/how-to-spin-up-a-free-linux-virtual-machine-on-google-cloud-platform-1olj',
+          icon: 'dev',
+        },
+      ],
+      imageObj: {
+        alt: 'Cloud Computing Concept Image',
+        src: '/assets/images/projects/cloud-computing.jpg',
+        attributionLabel: 'Image by Vecteezy',
+        attributionUrl: 'https://www.vecteezy.com/free-vector/cloud-computing',
+      },
+    },
+    {
+      title: 'Set Up Notifications with Python',
+      linkObjs: [
+        {
+          label: 'Read Post',
+          url: 'https://dev.to/danielcaballero88/how-to-set-up-email-and-message-notifications-for-a-python-application-5f3l',
+          icon: 'dev',
+        },
+      ],
+      imageObj: {
+        alt: 'Notifications Concept Image',
+        src: '/assets/images/projects/notifications.jpg',
+        attributionLabel: 'Image by Vecteezy',
+        attributionUrl: 'https://www.vecteezy.com/free-photos',
+      },
+    },
+    {
+      title: 'Deploy to a Linux VM',
+      linkObjs: [
+        {
+          label: 'Read Post',
+          url: 'https://dev.to/danielcaballero88/how-to-deploy-your-program-to-run-periodically-in-a-linux-vm-in-gcp-compute-engine-4km6',
+          icon: 'dev',
+        },
+      ],
+      imageObj: {
+        alt: 'Deploy to Cloud Concept',
+        src: '/assets/images/projects/deploy-to-cloud.jpg',
+        attributionLabel: 'Image by Vecteezy',
+        attributionUrl: 'https://www.vecteezy.com/free-vector/cloud-computing',
       },
     },
   ];
